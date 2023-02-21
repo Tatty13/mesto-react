633e274dc8655b30c897b1f90354d1b006ad33f3
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-02-21 15:09:26 +0300

    feat: port formsConfigData and utils files

---
+++ mesto/src/utils/formsConfigData.js
@@ -0,0 +1,10 @@
+const formsConfig = {
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__submit-btn',
+  inactiveButtonClass: 'form__submit-btn_disabled',
+  inputErrorClassPrefix: '-input-error',
+  errorClass: 'form__input-error_active'
+}
+
+export default formsConfig;
---
+++ mesto/src/utils/utils.js
@@ -0,0 +1,9 @@
+/**
+ * @param {HTMLElement} elem 
+ * @param {string} text 
+ */
+function setTextToELem(elem, text) {
+  elem.textContent = text;
+}
+
+export { setTextToELem }
