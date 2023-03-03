2826fb6ff28456ef8149361f80a2495533a68acd
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-03-03 16:52:25 +0300

    refactor: render cards in the <ul>; add likes count; add several props instead of one card prop

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,18 +1,18 @@
-function Card({card, onCardClick}) {
+function Card({name, link, likesCount, onCardClick}) {
 
   function handleClick() {
-    onCardClick(card);
+    onCardClick({name, link});
   }
 
   return (
     <li className="card">
       <div className="card__img-wrap">
-        <img className="card__img" src={card.link} alt={card.name} onClick={handleClick}/>
+        <img className="card__img" src={link} alt={name} onClick={handleClick}/>
       </div>
-      <h2 className="card__title">{card.name}</h2>
+      <h2 className="card__title">{name}</h2>
       <div className="card__like">
         <button className="card__like-btn" type="button" aria-label="Нравится"></button>
-        <span className="card__like-count"></span>
+        <span className="card__like-count">{likesCount || ''}</span>
       </div>
       <button className="card__delete-btn" type="button" aria-label="Удалить"></button>
     </li>
--- src/components/Main.js
+++ src/components/Main.js
@@ -18,10 +18,7 @@ function Main({onEditAvatar, onEditProfile, onAddPlace, onCardClick, onError}) {
           setUserAvatar(avatar);
           setCards([
             ...cards,
-            ...cardsData.map(data => (
-              <Card card={data} key={data._id} onCardClick={onCardClick}/>
-              )
-            )
+            ...cardsData
           ])
         })
         .catch(err => onError(err))
@@ -41,7 +38,14 @@ function Main({onEditAvatar, onEditProfile, onAddPlace, onCardClick, onError}) {
         <button className="profile__add-btn" type="button" aria-label="Добавить" onClick={onAddPlace}></button>
       </section>
       <section className="cards" aria-label="Место">
-        <ul className="cards__list">{cards}</ul>
+        <ul className="cards__list">{cards.map(card => (
+          <Card 
+            key={card._id}
+            name={card.name}
+            link={card.link}
+            likesCount={card.likes.length}
+            onCardClick={onCardClick}/>
+        ))}</ul>
       </section>
     </main>
   )
